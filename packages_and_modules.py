6fd5b2852ebee7a7dd7d5f1ca9a9ceb7f5b08e0c
armankoca <armankoca@protonmail.com>
2021-07-03 17:26:14 +0300

    END OF 2021-07-03

--- packages_and_modules.py
+++ packages_and_modules.py
@@ -1,6 +1,14 @@
+import sys
 import urllib
 import urllib.request
 
+
+# Both packages and modules are type of module.
 print(type(urllib))
 print(type(urllib.request))
-print(urllib.__path__)
+
+print(urllib.__path__)  # Returns the path of a module
+print(sys.path)         # Python searches modules inside the directories listed in sys.path
+print(sys.path[0])      # First item in the list is the current directory.
+# We can append a directory to sys.path or...
+# We can set the directory inside the PYTHONPATH environment variable.
